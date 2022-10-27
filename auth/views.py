9a1da72eac47e47333f151cbfba3a9211e4c58f9
Marina Oliveira <s5227111@bournemouth.ac.uk>
2022-10-27 12:25:50 +0100

    dunder init

--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,7 @@
 # MANAGE APP.PY URLs
 from flask import Blueprint, render_template, url_for
 
-auth_bp = Blueprint("auth", __name__, template_folder="templates/auth", static_folder="static")
+auth_bp = Blueprint("auth", __name__, template_folder="templates", static_folder="static")
 
 @auth_bp.route("/")
 def index():
